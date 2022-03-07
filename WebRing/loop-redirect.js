6bec283836a622ba3c9ac43359f370d4940db0ad
Gray <DrEarlGray@gmail.com>
2022-03-07 08:00:48 -0500

    Loop redirect to next index

--- WebRing/loop-redirect.js
+++ WebRing/loop-redirect.js
@@ -1,5 +1,22 @@
-let thisSite = window.location.href;
-let referrer = document.referrer;
+import { sites } from "./sites.js";
+
+let thisSite = document.referrer;
 console.log(thisSite);
-console.log(referrer);
-window.location.href = 'https://Example.com/';
+
+
+let thisIndex;
+let i;
+for (i = 0; i < sites.length; i++) {
+  if (thisSite.startsWith(sites[i])) {
+    thisIndex = i;
+    break;
+  }
+}
+
+if (thisIndex == null) {
+  window.location.href = 'https://graycot.com/webring/error.html';
+}
+else{
+  let nextIndex = (thisIndex+1 >= sites.length) ? 0 : thisIndex+1;
+  window.location.href = sites[nextIndex];
+}
