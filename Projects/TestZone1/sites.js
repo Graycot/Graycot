a12312e46b399973cd2e07499fe2a9058333d8c9
Gray <DrEarlGray@gmail.com>
2022-03-07 04:57:21 -0500

    New Cozy-ring Code

---
+++ Projects/TestZone1/sites.js
@@ -0,0 +1,12 @@
+function sitesMod() {
+  const sites = [
+    'https://windigo.neocities.org/',
+    'https://graystea.neocities.org/',
+    'http://localhost:8080/'
+  ];
+  return sites;
+}
+export { sitesMod };
+
+
+
