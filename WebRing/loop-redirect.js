ef57488e5927431324ff465049a0e36ec7afefd5
Gray <DrEarlGray@gmail.com>
2022-03-07 08:17:26 -0500

    webring: added query string capability for redirect links.

--- WebRing/loop-redirect.js
+++ WebRing/loop-redirect.js
@@ -3,7 +3,6 @@ import { sites } from "./sites.js";
 let thisSite = document.referrer;
 console.log(thisSite);
 
-
 let thisIndex;
 let i;
 for (i = 0; i < sites.length; i++) {
@@ -13,6 +12,17 @@ for (i = 0; i < sites.length; i++) {
   }
 }
 
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+// Get the value of "action" in eg "https://example.com/?action=some_value"
+let value = params.action; // "some_value"
+console.log(value);
+
+
+
+/*
+
 if (thisIndex == null) {
   window.location.href = 'https://graycot.com/webring/error.html';
 }
@@ -20,3 +30,5 @@ else{
   let nextIndex = (thisIndex+1 >= sites.length) ? 0 : thisIndex+1;
   window.location.href = sites[nextIndex];
 }
+
+*/
