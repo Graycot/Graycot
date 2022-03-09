b345c4f4d92b03af8c71be7524a25279cffe2621
Gray <DrEarlGray@gmail.com>
2022-03-09 17:07:33 -0500

    Remove Localhost site.js

--- WebRing/sites.js
+++ WebRing/sites.js
@@ -2,8 +2,7 @@ export const sites = [
   'https://windigo.neocities.org/',
   'https://graystea.neocities.org/',
   'https://graycot.com/',
-  'https://swiftyshq.neocities.org/',
-  'http://localhost:8080/'
+  'https://swiftyshq.neocities.org/'
 ];
 
 
