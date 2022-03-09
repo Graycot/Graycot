eca5ae550cd03ebf881f7141fd2bc0f4bf21429e
Gray <DrEarlGray@gmail.com>
2022-03-09 15:52:03 -0500

    Merge branch 'main' of https://github.com/Graycot/Graycot

--- JS/main.js
+++ JS/main.js
@@ -6,45 +6,39 @@ class main extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
 
-   <main class="main-element" id="demo"> 
-   
-<div class="grid-container">
+<main class="main-element" id="demo"> 
+  <div class="grid-container">
 
-  <a href="/Projects/TestZone1/index.html" target="_blank"><div class="grid-item">JavaScript Testing Zone 1</div></a>
-  <a href="/Projects/TestZone2/index.html" target="_blank"><div class="grid-item">JavaScript Testing Zone 2</div></a>
-  <a href="/Projects/TestZone3/index.html" target="_blank"><div class="grid-item">JavaScript Testing Zone 3</div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
+  <a href="../WebRing/index.html" target=""><div class="grid-item">Loop Ring</div></a>
+  <a href="/Projects/TestZone1/index.html" target=""><div class="grid-item">JavaScript Testing Zone 1</div></a>
+  <a href="/Projects/TestZone2/index.html" target=""><div class="grid-item">JavaScript Testing Zone 2</div></a>
+  <a href="/Projects/TestZone3/index.html" target=""><div class="grid-item">JavaScript Testing Zone 3</div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
 
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
 
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
 
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
-  <a href="" target="_blank"><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
+  <a href="" target=""><div class="grid-item"></div></a>
 
-  
-
-
-</div>
-   
-   
-   </main>
+  </div>
+</main>
     `;
   }
 }
