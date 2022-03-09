925fb90d4b0cdc58c3d6fee49b9182b9b4f1df28
Gray <DrEarlGray@gmail.com>
2022-03-09 15:52:03 -0500

    Merge branch 'main' of https://github.com/Graycot/Graycot

--- WebRing/sites.js
+++ WebRing/sites.js
@@ -2,6 +2,7 @@ export const sites = [
   'https://windigo.neocities.org/',
   'https://graystea.neocities.org/',
   'https://graycot.com/',
+  'https://swiftyshq.neocities.org/',
   'http://localhost:8080/'
 ];
 
