e25436839cb4f810e4fd10302e24862f37a9e2ae
Gray <DrEarlGray@gmail.com>
2022-03-13 01:07:55 -0500

    Minor webring update

--- WebRing/sites.js
+++ WebRing/sites.js
@@ -1,7 +1,7 @@
 export const sites = [
   'https://windigo.neocities.org/',
   'https://graystea.neocities.org/',
-  'https://graycot.com/',
+  'https://graycot.com/webring/',
   'https://swiftyshq.neocities.org/'
 ];
 
