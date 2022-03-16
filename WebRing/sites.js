589ed1ede4c89b4c3e458797e8bc2f77b6d585e6
Gray <DrEarlGray@gmail.com>
2022-03-16 04:49:33 -0400

    Adding Temais to LoopRing

--- WebRing/sites.js
+++ WebRing/sites.js
@@ -2,7 +2,8 @@ export const sites = [
   'https://graycot.com/webring/',
   'https://graystea.neocities.org/',
   'https://windigo.neocities.org/',
-  'https://swiftyshq.neocities.org/'
+  'https://swiftyshq.neocities.org/',
+  'https://temaisgame.neocities.org/'
 ];
 
 
